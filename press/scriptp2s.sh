7f0fc0555702f8e04745be2a5083eed3ba94e429
deusanyjunior <deusanyjunior@gmail.com>
2015-03-31 00:24:53 -0400

    pressentation through the cloud added

---
+++ press/scriptp2s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$1" ]
+ then
+	echo "No file supplied"
+	exit 1
+fi
+rm -R slides;
+mkdir slides;
+pdfseparate  $1 slides/slide%d.pdf;
+SLIDES=slides/*.pdf;
+suffix=".pdf";
+for f in $SLIDES 
+do
+	filename=${f%$suffix}
+	pdf2svg $f $filename.svg
+	rm $f
+done
