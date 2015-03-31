322a136f0e664bc30b169b38606696340a92f548
deusanyjunior <deusanyjunior@gmail.com>
2015-03-31 12:01:45 -0400

    press using only javascript

--- press/scriptpdf2svg.sh
+++ press/scriptpdf2svg.sh
@@ -2,14 +2,16 @@
 if [ -z "$1" ]
  then
 	echo "No file supplied"
-	exit 1
+exit 1
 fi
 rm -R slides;
 mkdir slides;
+touch slides/index.html;
+
 pdfseparate  $1 slides/slide%d.pdf;
 SLIDES=slides/*.pdf;
 suffix=".pdf";
-for f in $SLIDES 
+for f in $SLIDES
 do
 	filename=${f%$suffix}
 	pdf2svg $f $filename.svg
